328ea1a62eb6ffa8a3bf7f783fb7999a88754acc
Josh Abraham <sinisterpatrician@gmail.com>
2017-06-01 17:44:15 -0400

    Add opts; -h working

--- wordsorter.c
+++ wordsorter.c
@@ -1,10 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
 
 #include "wordsorter.h"
 
 int main (int argc, char **argv)
 {
-    usage(argv[0]);
+    int opt = 0;
+    int c_val = 0;
+    bool r_flg = 0, n_flg = 0, l_flg = 0, s_flg = 0, a_flg = 0, u_flg = 0, h_flg = 0;
+
+    while ((opt = getopt(argc, argv, OPTS)) != -1) {
+        switch (opt) {
+            case 'c':
+                c_val = atoi(optarg);
+                break;
+            case 'r':
+                r_flg = true;
+                break;
+            case 'n':
+                n_flg = true;
+                break;
+            case 'l':
+                l_flg = true;
+                break;
+            case 's':
+                s_flg = true;
+                break;
+            case 'a':
+                a_flg = true;
+                break;
+            case 'u':
+                a_flg = true;
+                break;
+            case 'h':
+                h_flg = true;
+                break;
+        }
+    }
+
+    if (h_flg) {
+        usage(argv[0]);
+        return 0;
+    }
+
     return 0;
 }
 
--- wordsorter.h
+++ wordsorter.h
@@ -1,6 +1,9 @@
 #ifndef WS_H
 #define WS_H
 
+/* Definitions */
+#define OPTS "c:rnlsauh"
+
 /* Prototypes */
 void usage(char *p_name);
 
